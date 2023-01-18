772da2df2a63063c843819cdee252daaf124f0c9
Chaoz <rudiwido99@gmail.com>
2023-01-18 21:57:32 +0700

    menambah fitur tampil gambar

--- resources/views/frontend/blog.blade.php
+++ resources/views/frontend/blog.blade.php
@@ -54,8 +54,8 @@
                         <div class="col-md-4 mb-3">
                             <div class="card">
                                 <div class="position-absolute px-3 py-2 text-white" style="background: rgb(0,0,0,0.7)"><a href="/posts?category={{ $item->category->slug }}" class="text-decoration-none text-white">{{ $item->category->name }}</a></div>
-                                @if ($blog->image)
-                                    <img src="{{ asset('storage/'. $blog->image) }}" alt="{{ $blog->category->name }}" class="img-fluid mt-3">
+                                @if ($item->image)
+                                    <img src="{{ asset('storage/'. $item->image) }}" alt="{{ $item->category->name }}" class="img-fluid mt-3">
                                 @else
                                     <img src="https://source.unsplash.com/400x300?{{ $item->category->name }}" class="card-img-top" alt="{{ $item->category->name }}">
                                 @endif
--- resources/views/frontend/post.blade.php
+++ resources/views/frontend/post.blade.php
@@ -9,7 +9,13 @@
                         <a href="/posts?category={{ $articles->category->slug }}" class="text-decoration-none">
                         {{ $articles->category->name }}</a> 
                     </p>
-                    <img src="https://source.unsplash.com/1200x400/?{{ $articles->category->name }}" alt="{{ $articles->category->name }}" class="img-fluid">
+                    @if ($articles->image)
+                        <div style="max-height: 350px; overflow:hidden;">
+                            <img src="{{ asset('storage/'. $articles->image) }}" alt="{{ $articles->category->name }}" class="img-fluid mt-3">
+                        </div>
+                    @else
+                        <img src="https://source.unsplash.com/1200x400/?{{ $articles->category->name }}" alt="{{ $articles->category->name }}" class="img-fluid">
+                    @endif
 
                     <article class="my-3">
                         {!! $articles->desc !!}
