69e08cd8f07824607bdb8a6ef0ccb5e325d7cc4a
Chaoz <rudiwido99@gmail.com>
2023-01-21 18:47:45 +0700

    membuat tampilan frontend

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,68 @@
 @extends('template.front.main')
 @section('title', 'Beranda')
 @section('content')
-    <div class="container">
+{{-- Hero Section --}}
+<section class="hero" id="home">
+    <main class="content">
+        <h1>Mari Nikmati Secangkir <span>Kopi</span></h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum, nihil.</p>
+        <a href="#" class="cta">Beli Sekarang</a>
+    </main>
+</section>
+{{-- End Hero Section --}}
+
+{{-- About Section --}}
+<section id="about" class="about">
+    <h2><span>Tentang</span> Kami</h2>
+    <div class="row">
+        <div class="about-img">
+            <img src="image/tentang.jpg" alt="Tentang Kami">
+        </div>
+        <div class="content">
+            <h3>Kenapa memilih kopi kami</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim possimus delectus provident, eum sit similique.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum voluptatibus est totam esse facere similique deserunt vitae atque pariatur maxime.</p>
+        </div>
+    </div>
+</section>
+{{-- End About Section --}}
+
+{{-- Menu Section --}}
+<section id="menu" class="menu">
+    <h2><span>Menu</span> Kami</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium pariatur ad officia amet deserunt! Suscipit.</p>
+    <div class="menu-row">
+        <div class="menu-card">
+            <img src="image/menu/1.jpg" alt="Espresso" class="menu-card-img">
+            <h3 class="menu-card-title">- Espresso -</h3>
+            <p class="menu-card-price">IDR 15K</p>
+        </div>
+        <div class="menu-card">
+            <img src="image/menu/1.jpg" alt="Espresso" class="menu-card-img">
+            <h3 class="menu-card-title">- Espresso -</h3>
+            <p class="menu-card-price">IDR 15K</p>
+        </div>
+        <div class="menu-card">
+            <img src="image/menu/1.jpg" alt="Espresso" class="menu-card-img">
+            <h3 class="menu-card-title">- Espresso -</h3>
+            <p class="menu-card-price">IDR 15K</p>
+        </div>
+        <div class="menu-card">
+            <img src="image/menu/1.jpg" alt="Espresso" class="menu-card-img">
+            <h3 class="menu-card-title">- Espresso -</h3>
+            <p class="menu-card-price">IDR 15K</p>
+        </div>
+    </div>
+</section>
+{{-- End Menu Section --}}
+
+@endsection
+
+
+
+
+    {{-- <div class="container">
         <div class="row my-4">
             <h1>Halaman Home</h1>
         </div>
-    </div>
-@endsection
+    </div> --}}
--- resources/views/template/front/main.blade.php
+++ resources/views/template/front/main.blade.php
@@ -3,19 +3,27 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('title') | Sport Store</title>
+    <title>@yield('title') | Kopi Kenangan Kita</title>
     
     {{-- Bootstrap Style & Icon --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" 
     rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
   
+    {{-- Fonts --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,700;1,700&display=swap" rel="stylesheet">
+
+    {{-- Feather-icons --}}
+    <script src="https://unpkg.com/feather-icons"></script>
+    
     {{-- Custom CSS --}}
     <link rel="stylesheet" href="/css/style.css">
 
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    {{-- <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container">
             <a class="navbar-brand" href="#">Sport Store</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -61,11 +69,41 @@
             </ul>
             </div>
         </div>
+    </nav> --}}
+
+    {{-- Navbar --}}
+    <nav class="navbar">
+      <a href="#" class="navbar-logo">Kenangan<span>Kita</span>.</a>
+
+      <div class="navbar-nav">
+        <a href="#home">Home</a>
+        <a href="#about">Tentang Kami</a>
+        <a href="#menu">Menu</a>
+        <a href="#">Kontak</a>
+        <a href="#">Artikel</a>
+      </div>
+
+      <div class="navbar-extra">
+        <a href="#" id="search"><i data-feather="search"></i></a>
+        <a href="#" id="shopping-cart"><i data-feather="shopping-cart"></i></a>
+        <a href="#" id="hamburger-menu"><i data-feather="menu"></i></a>
+      </div>
     </nav>
+    {{-- Navbar End --}}
+
 
     @yield('content')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" 
     integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+    {{-- Feather Icon --}}
+    <script>
+      feather.replace()
+    </script>
+
+    {{-- My Javascript  --}}
+    <script src="js/script.js"></script>
+
   </body>
 </html>
